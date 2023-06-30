8c967d48a8e9b550235cdbd084e7923a022c3c68
Marco Salazar <122067295+Marco-str@users.noreply.github.com>
2023-06-30 15:25:40 -0300

    Todos los timpos de variables mostrados en la consola

---
+++ com/RepoMarcoGithub/Main.java
@@ -0,0 +1,42 @@
+package com.RepoMarcoGithub;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //1 numericos
+
+        //1.1 enteros
+        byte variable1 = 40;
+        System.out.println(variable1);
+
+        short variable2 = 5000;
+        System.out.println(variable2);
+
+        int variable3 = 1000000000;
+        System.out.println(variable3);
+
+        long variable4 = 1000000000;
+        System.out.println(variable4);
+
+        //1.2 decimales
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+
+        double variable6 = 15.5000000000000d;
+        System.out.println(variable6);
+
+        //booleanos
+        boolean variable7 = true;
+        System.out.println(variable7);
+
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        //texto
+        char variable9 = 'a';
+        System.out.println(variable9);
+
+        String variable10 = "hola mundo del mundo mundial";
+        System.out.println(variable10);
+    }
+}
